2d7feaa65b5b08c7f22c15f507d4fecad74bdf50
Adam Wojcik <20500@student.ans-elblag.pl>
2024-10-21 11:13:27 +0200

    lab4 z1v2

--- WojcikAdamLab4Node/index.js
+++ WojcikAdamLab4Node/index.js
@@ -1,29 +1,69 @@
 // ZADANIE 1. Wywołania zwrotne
 
+// v1
+// const http = require('http');
+// const fs = require('fs');
+//
+// http.createServer(function(request, response) {
+//     if (request.url === '/') {
+//         fs.readFile('./titles.json', function(err, data) {
+//             if (err) {
+//                 console.error(err);
+//                 response.end('titles.json error');
+//             }
+//             else {
+//                 const titles = JSON.parse(data.toString());
+//                 fs.readFile('./teamplate.html', function(err, data) {
+//                     if (err) {
+//                         console.error(err);
+//                         response.end('teamplate.html error');
+//                     } else {
+//                         const tmpl = data.toString();
+//                         let html = tmpl.replace('%', titles.join('</li><li>'));
+//                         response.writeHead(200, {'Content-Type': 'text/html'});
+//                         response.end(html);
+//                     }
+//                 });
+//             }
+//         });
+//     }
+// }).listen(8000, "127.0.0.1");
+
+// v2
 const http = require('http');
 const fs = require('fs');
 
-http.createServer(function(request, response) {
-    if (request.url === '/') {
-        fs.readFile('./titles.json', function(err, data) {
-            if (err) {
-                console.error(err);
-                response.end('titles.json error');
-            }
-            else {
-                const titles = JSON.parse(data.toString());
-                fs.readFile('./teamplate.html', function(err, data) {
-                    if (err) {
-                        console.error(err);
-                        response.end('teamplate.html error');
-                    } else {
-                        const tmpl = data.toString();
-                        const html = tmpl.replace('%', titles.join('</li><li>'));
-                        response.writeHead(200, {'Content-Type': 'text/html'});
-                        response.end(html);
-                    }
-                });
-            }
-        });
-    }
+const server = http.createServer(function (req, res) {
+    getTitles(res);
 }).listen(8000, "127.0.0.1");
+
+function getTitles(res) {
+    fs.readFile('./titles.json', function (err, data) {
+        if (err) {
+            hadError(err, res);
+        } else {
+            getTeamplate(JSON.parse(data.toString()), res);
+        }
+    });
+}
+
+function getTeamplate(titles, res) {
+    fs.readFile('./teamplate.html', function (err, data) {
+        if (err) {
+            hadError(err, res);
+        } else {
+            formatHtml(titles, data.toString(), res);
+        }
+    });
+}
+
+function formatHtml(titles, tmpl, res) {
+    let html = tmpl.replace('%', titles.join('</li><li>'));
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(html);
+}
+
+function hadError(err, res) {
+    console.error(res);
+    res.end('Error');
+}
