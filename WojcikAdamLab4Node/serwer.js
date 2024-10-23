83c0f027e059e95a3ce28378a3fc7ab8d4f7492b
Adam Wojcik <20500@student.ans-elblag.pl>
2024-10-23 08:41:57 +0200

    lab4

--- WojcikAdamLab4Node/serwer.js
+++ WojcikAdamLab4Node/serwer.js
@@ -1,5 +1,3 @@
-// ZADANIE 2. Emiter zdarzeń.
-
 function Watcher(watchDir, processedDir) {
     this.watchDir = watchDir;
     this.processedDir = processedDir;
@@ -8,34 +6,33 @@ function Watcher(watchDir, processedDir) {
 const events = require('events');
 const util = require('util');
 util.inherits(Watcher, events.EventEmitter);
-
 const fs = require('fs');
+
 const watchDir = './watch';
 const processedDir = './done';
 
 Watcher.prototype.watch = function() {
     const watcher = this;
-    fs.readdir(this.watchDir, function (err, files) {
+    fs.readdir(this.watchDir, function(err, files) {
         if (err) throw err;
-        for (const index of files) {
+        for (let index in files) {
             watcher.emit('process', files[index]);
         }
     });
-}
+};
 
 Watcher.prototype.start = function() {
     const watcher = this;
-    fs.watchFile(watchDir, function () {
+    fs.watchFile(watchDir, function() {
         watcher.watch();
     });
-}
+};
 
 const watcher = new Watcher(watchDir, processedDir);
-
-watcher.on('process', function(file) {
+watcher.on('process', function process(file) {
     const watchFile = this.watchDir + '/' + file;
     const processedFile = this.processedDir + '/' + file.toLowerCase();
-    fs.rename(watchFile, processedFile, function (err) {
+    fs.rename(watchFile, processedFile, function(err) {
         if (err) throw err;
     });
 });
