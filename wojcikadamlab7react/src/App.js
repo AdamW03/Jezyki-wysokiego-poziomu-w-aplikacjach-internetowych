eb1604505f615c1bc0da4fbd171a05ddfff6e859
Adam Wojcik <wojcikadam.03@gmail.com>
2024-11-18 03:02:09 +0100

    lab7

--- wojcikadamlab7react/src/App.js
+++ wojcikadamlab7react/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ReUseComponent from "./component/ReUseComponent";
+import TestUseState from "./component/TestUseState";
+import TestUseReducer from "./component/TestUseReducer";
+import TestUseEffect from "./component/TestUseEffect";
+import TestUseContext from "./component/TestUseContext";
+import TestUseRef from "./component/TestUseRef";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/*Zadanie nr 1*/}
+            <ReUseComponent  title="Zadanie 1">
+                <ReUseComponent title="Pierwszy komponent" content="To jest pierwszy komponent.">
+                    <button>Przycisk</button>
+                </ReUseComponent>
+
+                <ReUseComponent title="Drugi komponent" content="To jest drugi komponent.">
+                    <ul>
+                        <li>Element 1</li>
+                        <li>Element 2</li>
+                        <li>Element 3</li>
+                    </ul>
+                </ReUseComponent>
+
+
+                <ReUseComponent title="Trzeci komponent" content="To jest trzeci komponent.">
+                    <p>Inna zawartość tutaj.</p>
+                </ReUseComponent>
+            </ReUseComponent>
+
+            {/*Zadanie nr 2*/}
+            <ReUseComponent title="Zadanie 2">
+                <ReUseComponent title="Test useState">
+                    <TestUseState />
+                </ReUseComponent>
+                <ReUseComponent title="Test useReducer">
+                    <TestUseReducer />
+                </ReUseComponent>
+                <ReUseComponent title="Test useEffect">
+                    <TestUseEffect />
+                </ReUseComponent>
+                <ReUseComponent title="Test useContext">
+                    <TestUseContext />
+                </ReUseComponent>
+                <ReUseComponent title="Test useRef">
+                    <TestUseRef />
+                </ReUseComponent>
+            </ReUseComponent>
+        </div>
+    );
 }
 
 export default App;
