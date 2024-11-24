0f6dd8bfd866e806c59505c3b62c56fe8d9cd39f
Adam Wojcik <wojcikadam.03@gmail.com>
2024-11-24 17:25:02 +0100

    lab 8

---
+++ wojcikadamlab8react/src/modules/NiePrawidlowyKomponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NiePrawidlowyKomponent(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default NiePrawidlowyKomponent;
---
+++ wojcikadamlab8react/src/modules/PrawidlowyKomponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PrawidlowyKomponent(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default PrawidlowyKomponent;
