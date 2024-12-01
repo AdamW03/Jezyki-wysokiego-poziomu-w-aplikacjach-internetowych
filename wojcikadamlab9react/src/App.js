aaac9ba19c0ec4c605bcf8c29d5466d9f74d078b
Adam Wojcik <wojcikadam.03@gmail.com>
2024-12-02 00:56:08 +0100

    lab9

--- wojcikadamlab9react/src/App.js
+++ wojcikadamlab9react/src/App.js
@@ -13,21 +13,21 @@ function App() {
     <Router>
       <nav>
         <ul>
-          <li><Link to="/">O mnie</Link></li>
-          <li><Link to="/interests">Zainteresowania</Link></li>
-          <li><Link to="/favoritegame">Ulubiona Gra</Link></li>
-          <li><Link to="/lab7">Lab 7</Link></li>
-          <li><Link to="/lab8">Lab 8</Link></li>
-          <li><Link to="/contact">Kontakt</Link></li>
+          <li><Link to="/wojciklab9react/">O mnie</Link></li>
+          <li><Link to="/wojciklab9react/interests">Zainteresowania</Link></li>
+          <li><Link to="/wojciklab9react/favoritegame">Ulubiona Gra</Link></li>
+          <li><Link to="/wojciklab9react/lab7">Lab 7</Link></li>
+          <li><Link to="/wojciklab9react/lab8">Lab 8</Link></li>
+          <li><Link to="/wojciklab9react/contact">Kontakt</Link></li>
         </ul>
       </nav>
       <Routes>
-        <Route path="/" element={<AboutComponent />} />
-        <Route path="/interests" element={<InterestsComponent />} />
-        <Route path="/favoritegame" element={<FavoriteGameComponent />} />
-        <Route path="/lab7" element={<Lab7AppComponent />} />
-        <Route path="/lab8" element={<Lab8AppComponent />} />
-        <Route path="/contact" element={<ContactComponent />} />
+        <Route path="/wojciklab9react/" element={<AboutComponent />} />
+        <Route path="/wojciklab9react/interests" element={<InterestsComponent />} />
+        <Route path="/wojciklab9react/favoritegame" element={<FavoriteGameComponent />} />
+        <Route path="/wojciklab9react/lab7" element={<Lab7AppComponent />} />
+        <Route path="/wojciklab9react/lab8" element={<Lab8AppComponent />} />
+        <Route path="/wojciklab9react/contact" element={<ContactComponent />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
