d07720fb3794186fdb6c6b8f5b641adddbd6db00
Adam Wojcik <20500@student.ans-elblag.pl>
2025-01-13 11:42:58 +0100

    lab 13v2

--- wojcikadamlab13next/app/meals/page.js
+++ wojcikadamlab13next/app/meals/page.js
@@ -1,13 +1,15 @@
 import MealHeader from "@/components/MealHeader/MealHeader";
 import styles from "./page.module.css";
 import MealGrid from "@/components/meals/meal-grid";
+import {getMeals} from "@/lib/meals";
 
-export default function Meals() {
+export default async function Meals() {
+    const meals = await getMeals();
   return (
       <>
         <MealHeader />
         <main className={styles.main}>
-            <MealGrid meals={[]} />
+            <MealGrid meals={meals} />
         </main>
       </>
   );
